cf45f782c978ed462a5f26eb4d3ca205d5c48bcd
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-20 22:47:43 +0300

    Delete task1_1_4.sql

--- 3. task1_1_4/task1_1_4.sql
+++
@@ -1,9 +0,0 @@
---Сколько столбцов содержится в таблице [Order Details]?
---Примечание: если название таблицы состоит из двух или 
---более слов, то его нужно поместить в квадратные скобки. 
-
-Select count(*) as Count_Columns from INFORMATION_SCHEMA.COLUMNS WHERE 
-TABLE_CATALOG = 'Northwind' 
-AND TABLE_SCHEMA = 'dbo' 
-AND TABLE_NAME = 'Order Details'
-
