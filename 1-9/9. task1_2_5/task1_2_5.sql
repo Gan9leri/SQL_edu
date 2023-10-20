c5785a76d14dab1598329fcdf92465c54bc3e886
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-20 22:47:31 +0300

    Create task1_2_5.sql

---
+++ 1-9/9. task1_2_5/task1_2_5.sql
@@ -0,0 +1,5 @@
+--Выведите информацию о клиенте, который проживает в стране США и работает в должности 
+--"Marketing Assistant". Как зовут этого человека?
+
+Select ContactName from Customers where Country = 'USA' 
+and ContactTitle = 'Marketing Assistant'
