607ad39a220874d4b9ded3c4acf9ec4187f1fc8e
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-20 22:47:34 +0300

    Delete task1_1_2.sql

--- 1. task1_1_2/task1_1_2.sql
+++
@@ -1,4 +0,0 @@
---Раскройте список таблиц нашей базы данных. Сколько их?
-
-Select count(*) as Count_tables from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = 'dbo' 
-and TABLE_TYPE = 'BASE TABLE' and TABLE_NAME <> 'sysdiagrams'
