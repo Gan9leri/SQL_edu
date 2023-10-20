3d9690b7484812365a351285f25699258f4a841f
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-20 22:47:54 +0300

    Delete task1_2_5.sql

--- 9. task1_2_5/task1_2_5.sql
+++
@@ -1,5 +0,0 @@
---Выведите информацию о клиенте, который проживает в стране США и работает в должности 
---"Marketing Assistant". Как зовут этого человека?
-
-Select ContactName from Customers where Country = 'USA' 
-and ContactTitle = 'Marketing Assistant'
