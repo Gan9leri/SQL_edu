fd5429b0e22d0a478a49c0134c1c8996e95a67e2
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-21 15:48:22 +0300

    Delete task1_2_6.sql

--- 10-19/10 task1_2_6/task1_2_6.sql
+++
@@ -1,7 +0,0 @@
---Выведите продукты, которые стоят больше 100 долларов или называются Chai. 
---Столбцы для вывода: название продукта и цена продукта. Сколько продуктов вывел запрос?
-select count(*) as Count_out from 
-(Select ProductName, UnitPrice from Products where UnitPrice > 100 or ProductName = 'Chai') 
-as Data_out
-
-
